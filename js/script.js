7199444d196d24da33dabd5df454d3daa79e1c0d
Muhammadsalam Musaev <muhammadsalam@list.ru>
2022-08-07 12:54:47 +0300

    popular slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 // Обработка клика на кнопку каталога в шапке
-let headerCatalogBtn = document.querySelector('.js-header-catalog');
+const headerCatalogBtn = document.querySelector('.js-header-catalog');
 headerCatalogBtn.addEventListener('click', (e) => e.target.classList.toggle('active'))
+
+// слайдер
+const popularSlider = new Swiper('.popular-inner', {
+    allowTouchMove: true,
+    slideActiveClass: 'popular-item-active',
+    effect: 'creative',
+    watchOverflow: true,
+    autoplay: {
+        delay: 5000,
+    },
+    pagination: {
+        el: '.popular-item__pagination',
+        clickable: true
+    }
+})
